c13c82a73a1c59c9bf8d66d1349d2ef7f1c19550
Kevin <kevinchu.work@gmail.com>
2020-07-17 17:46:06 +0800

    Tabs initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Asset
+import './App.scss';
+
+// Core
+class TabsWrapper extends React.Component {
+  
+  render() {
+    let tabs = this.props.listsAry.map((v, i) => 
+      <div 
+        className={`tabBtn ${this.props.activeTab === i ? "on":""}`} 
+        onClick={(e) => this.props.tabSwitching(i, e)} 
+        key={i}>
+        {v}
+      </div>
+    );
+    
+    return (
+      <div className="tabWrapper">
+        {tabs}
+      </div>    
+    )
+  }
 }
 
+class App extends React.Component {
+  
+  state = { activeTab: 0 };
+
+  tabSwitching = (data, e) => {
+    // e.preventDefault();
+    this.setState({activeTab: data});
+  }
+  
+  render() {
+    let listsAry = ['Tab 1', 'Tab 2', 'Tab 3'];
+
+    return (
+      <div>
+        <TabsWrapper 
+          listsAry={listsAry} 
+          tabSwitching={this.tabSwitching} 
+          activeTab={this.state.activeTab} />
+        <TabPanel>
+          
+        </TabPanel>
+      </div>
+    );
+  }
+
+}
 export default App;
